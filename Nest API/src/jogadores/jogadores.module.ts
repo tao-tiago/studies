c728d12643f656b3f3225a38617be461edcb8933
Tiago Pires <tao.pires@gmail.com>
2025-06-10 15:55:00 -0300

    feat: update repo

--- Nest API/src/jogadores/jogadores.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JogadoresController } from './jogadores.controller';
-
-@Module({
-  controllers: [JogadoresController],
-})
-export class JogadoresModule { }
